f71e61cd9a0daac6d9851d0a95c03b06c4e2bc71
simonemayers <simone21295@gmail.com>
2020-10-12 14:12:36 -0400

    Done

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,28 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
+  while i < array.length
+    collection.push(yield(array[i]))
+    i += 1
+  end
+  collection
+end
+
+def hello2(array)
+  i = 0 
   while i < array.length
     yield(array[i])
     i += 1
   end
-end
+end 
 
+# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+test_list = ["Tim", "Tom", "Jim"]
+
+another_list = hello2(test_list) {|n| "Hey #{n}"}
+
+compare_list = hello(test_list) {|n| "Hey #{n}"}
+
+puts another_list
